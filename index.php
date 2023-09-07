62de2b39e665f291e35b2d583340caa887119893
MicheleGresta <malachia_1997@hotmail.it>
2023-09-07 17:13:29 +0200

    bonus terminati

--- index.php
+++ index.php
@@ -40,6 +40,7 @@ $hotels = [
 
 ];
 
+
 ?>
 
 
@@ -58,37 +59,61 @@ $hotels = [
 
 <body>
     <div class="container py-5">
-        <select class="form-select" aria-label="Default select example">
-            <option value="0">Cerca tutti gli Hotel</option>
-            <option value="1">Cerca Hotel con Parcheggio</option>
-            <option value="2">Cerca Hotel senza Parcheggio</option>
-        </select>
+        <form method="GET" class="d-flex align-items-center">
+
+            <select class="form-select" aria-label="Default select example" name="parking-area">
+                <option value="null">Cerca tutti gli Hotel</option>
+                <option value="1">Cerca Hotel con Parcheggio</option>
+                <option value="0">Cerca Hotel senza Parcheggio</option>
+            </select>
+
+            <select class="form-select" aria-label="Default select example" name="ranking-area">
+                <option value="null">Cerca tutti gli Hotel</option>
+                <option value="1">Cerca Hotel con Minimo 1 Stella</option>
+                <option value="2">Cerca Hotel con Minimo 2 Stelle</option>
+                <option value="3">Cerca Hotel con Minimo 3 Stelle</option>
+                <option value="4">Cerca Hotel con Minimo 4 Stelle</option>
+                <option value="5">Cerca Hotel con 5 Stelle</option>
+            </select>
+
+            <div class="">
+                <button type="submit" class="btn btn-primary">Cerca</button>
+            </div>
+        </form>
     </div>
     <?php
     foreach ($hotels as $hotel) {
+        if (
+            !$_GET
+            || $hotel['parking'] == $_GET['parking-area'] && $hotel['vote'] >= $_GET['ranking-area']
+            || $hotel['parking'] == $_GET['parking-area'] && $_GET['ranking-area'] == 'null'
+            || $_GET['parking-area'] == 'null' && $hotel['vote'] >= $_GET['ranking-area']
+            || $_GET['parking-area'] == 'null' && $_GET['ranking-area'] == 'null'
+        ) {
 
     ?>
-        <div class="container">
+            <div class="container">
 
-            <ul class="list-group py-3">
-                <li class="list-group-item d-flex gap-3 align-items-center">
-                    <h4>Nome Hotel: </h4><?php echo $hotel['name'] ?>
-                </li>
-                <li class="list-group-item d-flex gap-3 align-items-center">
-                    <h4>Descrizione: </h4><?php echo $hotel['description'] ?>
-                </li>
-                <li class="list-group-item d-flex gap-3 align-items-center">
-                    <h4>Parcheggio in hotel: </h4><?php echo $hotel['parking'] ? 'Si' : 'No' ?>
-                </li>
-                <li class="list-group-item d-flex gap-3 align-items-center">
-                    <h4>Voto Hotel: </h4><?php echo $hotel['vote'] . " / 5" ?><i class="fa-solid fa-star color-star"></i>
-                </li>
-                <li class="list-group-item d-flex gap-3 align-items-center">
-                    <h4>Distanza dal centro città: </h4><?php echo $hotel['distance_to_center'] . " km" ?>
-                </li>
-            </ul>
-        </div>
+                <ul class="list-group py-3">
+                    <li class="list-group-item d-flex gap-3 align-items-center">
+                        <h4>Nome Hotel: </h4><?php echo $hotel['name'] ?>
+                    </li>
+                    <li class="list-group-item d-flex gap-3 align-items-center">
+                        <h4>Descrizione: </h4><?php echo $hotel['description'] ?>
+                    </li>
+                    <li class="list-group-item d-flex gap-3 align-items-center">
+                        <h4>Parcheggio in hotel: </h4><?php echo $hotel['parking'] ? 'Si' : 'No' ?>
+                    </li>
+                    <li class="list-group-item d-flex gap-3 align-items-center">
+                        <h4>Voto Hotel: </h4><?php echo $hotel['vote'] . " / 5" ?><i class="fa-solid fa-star color-star"></i>
+                    </li>
+                    <li class="list-group-item d-flex gap-3 align-items-center">
+                        <h4>Distanza dal centro città: </h4><?php echo $hotel['distance_to_center'] . " km" ?>
+                    </li>
+                </ul>
+            </div>
     <?php
+        }
     }
     ?>
 </body>
